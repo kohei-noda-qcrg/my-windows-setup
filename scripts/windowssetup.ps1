ea64c98d30e2bad86cdb323a2df1b62584aae904
Kohei Noda <ponserviceregist@gmail.com>
2024-05-25 21:31:45 +0900

    chore: Use winget.ps1 instead of windowssetup.ps1 on CI
    Because wsl2-ubuntu cannot install with CI windows container

--- scripts/windowssetup.ps1
+++ scripts/windowssetup.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = "Stop" # Stop to executing program when error is occure
 
 # run setup-wsl.ps1 and winget.ps1
 Write-Host "Start running setup-wsl.ps1"
-powershell -File $PSScriptRoot\setup-wsl.ps1
+. $PSScriptRoot\setup-wsl.ps1
 Write-Host "Finish running setup-wsl.ps1"
 Write-Host "Start running winget.ps1"
-powershell -File $PSScriptRoot\winget.ps1
+. $PSScriptRoot\winget.ps1
 Write-Host "Finish running winget.ps1"
