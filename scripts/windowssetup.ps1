64340dcca4302199ba420d54a7a7a00cf8feca7f
Kohei Noda <ponserviceregist@gmail.com>
2024-05-25 21:21:46 +0900

    chore: Fix script name

--- scripts/windowssetup.ps1
+++ scripts/windowssetup.ps1
@@ -1,9 +1,9 @@
 $ErrorActionPreference = "Stop" # Stop to executing program when error is occured
 
-# run setup-wsl.ps1 and setup-windows.ps1
+# run setup-wsl.ps1 and winget.ps1
 Write-Host "Start running setup-wsl.ps1"
-. .\setup-wsl.ps1
+powershell -File $PSScriptRoot\setup-wsl.ps1
 Write-Host "Finish running setup-wsl.ps1"
-Write-Host "Start running setup-windows.ps1"
-. .\setup-windows.ps1
-Write-Host "Finish running setup-windows.ps1"
+Write-Host "Start running winget.ps1"
+powershell -File $PSScriptRoot\winget.ps1
+Write-Host "Finish running winget.ps1"
