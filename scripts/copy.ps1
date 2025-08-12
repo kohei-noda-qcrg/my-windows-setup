5690668f64cbc6c26e1af1fa41f938fbe2007b02
Kohei Noda <k.noda@saver.jp>
2025-08-12 16:46:00 +0900

    scripts: refactor copy script

--- scripts/copy.ps1
+++ scripts/copy.ps1
@@ -1,3 +1,19 @@
-wsl --set-default Ubuntu
-wsl cp './ubuntu-setup.sh' ~
-wsl cp './writeubuntusettings.sh' ~
+$ErrorActionPreference = "Stop" # Stop to executing program when error is occured
+
+if ($args.Count -lt 1) {
+    Write-Host "Usage: $($MyInvocation.MyCommand.Name) <archlinux|ubuntu>"
+    exit 1
+}
+
+$distro = $args[0].ToLower()
+if ($distro -notin @('archlinux', 'ubuntu')) {
+    Write-Host "Unknown distribution: $distro"
+    Write-Host "Supported distributions: archlinux, ubuntu"
+    exit 1
+}
+
+wsl -d $distro bash -c "mkdir -p ~ && exit"
+$source = Join-Path $PSScriptRoot $distro
+$drive = (Get-Item $source).PSDrive.Name.ToLower()
+$wslSource = "/mnt/$drive" + ($source.Substring(2) -replace "\\", "/")
+wsl -d $distro bash -c "cp -r '$wslSource/'* ~/"
